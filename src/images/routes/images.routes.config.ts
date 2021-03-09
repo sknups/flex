d453ec5ed8e7e767893c3374def337fb0f3bf826
Emanuel Coelho <1029702+emanuelcoelho1986@users.noreply.github.com>
2021-03-09 07:28:07 +0000

    [DRM-64] Added old files to mimic the old certificate render
    - Mimic the current system
    - Next will be move to a better approach

---
+++ src/images/routes/images.routes.config.ts
@@ -0,0 +1,46 @@
+import {CommonRoutesConfig} from "../../common/common.routes.config";
+import express from "express";
+import {ImagesController} from "../controllers/images.controller";
+import logger from "winston";
+
+export class ImagesRoutesConfig extends CommonRoutesConfig {
+
+    private readonly imagesController: ImagesController;
+
+    constructor(app: express.Application) {
+        super(app, 'Images Routes');
+
+        this.imagesController = new ImagesController();
+    }
+
+    configureRoutes(): express.Application {
+        // Root of Request
+        this.getApp().route(`/img`)
+            .get((req, res) => this.imagesController.index(req, res));
+
+        //app.get('/id/:id/img/:img/*', imgRouter);
+        /**
+        this.getApp()
+            .route('/img/:id/**')
+            .get((req, res) => this.handleImageRequest(req, res));
+         **/
+
+        // src='http://localhost:3000/img/' + data.id + '/img/default/cert'
+        this.getApp()
+            .route('/img/:itemId/img/:imageType/**')
+            .get((req, res) => this.handleImageRequest(req, res));
+
+        return this.getApp();
+    }
+
+    /**
+     * We might have multiple routes handling the image in the same way
+     * @param request
+     * @param response
+     * @private
+     */
+    private handleImageRequest(request: express.Request, response: express.Response) {
+        logger.info(`ImagesRoutesConfig.handleImageRequest from: ${request.params.itemId} with image: ${request.params.imageType}`);
+        this.imagesController.getImage(request, response);
+    }
+}
