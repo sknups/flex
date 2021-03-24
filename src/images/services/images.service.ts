66a03e58f8863bd2d7c29b33f8bd302afd478b12
Henrique Delgado <32763987+henriquedelgado@users.noreply.github.com>
2021-03-24 13:11:49 +0000

    main added getSkuImage endpoint

--- src/images/services/images.service.ts
+++ src/images/services/images.service.ts
@@ -2,6 +2,7 @@ import {CertificateDTO} from "../../certificates/services/certificates.service";
 import {StringUtils} from "../../utils/string.utils";
 import logger from "winston";
 import {BrandTemplate} from "../../templates/BrandTemplate";
+import * as fs from "fs";
 
 export class ImagesService {
 
@@ -26,5 +27,23 @@ export class ImagesService {
 
             return templateController.renderTemplate(fromCertificate, use);
         }
+    }
+
+    async getSkuImage(cert: CertificateDTO): Promise<Buffer> {
+        return new Promise<Buffer>((accept, reject) => {
+            const brandCode = cert.brandCode;
+            const skuCode = cert.stockKeepingUnitCode;
+            const image = cert.image;
+
+            logger.info(`ImagesService.getSkuImage: Will load image with brandCode: ${brandCode}, skuCode: ${skuCode}, imageName: ${image}`);
+
+            const imagePath = `./static/assets/brands/${brandCode}/99999999/${skuCode}/v1/${image}.png`;
+
+            logger.info(imagePath);
+            fs.readFile(imagePath, (err, data) => {
+                if (err) throw err; // Fail if the file can't be read.
+                accept(data);
+            });
+        });
     }
 }
