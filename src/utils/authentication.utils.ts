2c624e0e1a0edf17d3c33e80eb1864c9f6e69c99
Henrique Delgado <henrique.delgado@polarising.com>
2021-04-08 15:03:57 +0100

    DRM-65 add bearer token to get cert and created authentication.utils.ts

---
+++ src/utils/authentication.utils.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import logger from "winston";
+
+export class AuthenticationUtils {
+    static getServiceBearerToken(serviceUrl: string): Promise<string> {
+        const metadataUrl = `http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${serviceUrl}`;
+        const options = {
+            headers: {
+                'Metadata-Flavor': 'Google'
+            }
+        };
+
+        return axios.get(metadataUrl, options)
+            .then((res: any) => {
+                logger.debug(res.data);
+                return res.data;
+            }).catch((error: any) => {
+                logger.error(error);
+                throw new Error(error);
+            });
+    }
+}
