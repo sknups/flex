5bfaace06e81b6dab892e9cc96b51d3c2a4c6a1c
Henrique Delgado <32763987+henriquedelgado@users.noreply.github.com>
2021-04-08 15:33:56 +0100

    DRM-65 testing chained calls in skuService

--- src/skus/services/skus.service.ts
+++ src/skus/services/skus.service.ts
@@ -1,6 +1,5 @@
 import axios, {AxiosResponse} from "axios";
 import logger from "winston";
-import {AuthenticationUtils} from "../../utils/authentication.utils";
 
 export interface SkuDTO {
     // FIX DTO
@@ -21,17 +20,36 @@ export class SkusService {
      * Get a sku from skuCode
      * @param withCode
      */
-    async getSku(withCode: string): Promise<AxiosResponse<SkuDTO>> {
+    getSku(withCode: string): Promise<AxiosResponse<SkuDTO>> {
         logger.info(`SkusService.getSku withCode:${withCode} from ${this.serverUrl}/v1/api/skus/${withCode}`);
-        const url = `${this.serverUrl}/v1/api/skus/${withCode}`;
-        const bearerToken = await AuthenticationUtils.getServiceBearerToken(url);
 
-        const drmOptions = {
+        const url = `${this.serverUrl}/v1/api/skus/${withCode}`;
+        const metadataUrl = `http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${url}`;
+        const options = {
             headers: {
-                Authorization: `Bearer ${bearerToken}`,
+                'Metadata-Flavor': 'Google'
             }
         };
 
-        return axios.get<SkuDTO>(url, drmOptions);
+        const bearerToken = axios.get(metadataUrl, options)
+            .then((res: any) => {
+                logger.info(res.data);
+                return res.data;
+            }).catch((error: any) => {
+                logger.error(error);
+                throw new Error(error);
+            });
+
+        return bearerToken.then((token: any) => {
+            const drmOptions = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            };
+            return axios.get<SkuDTO>(url, drmOptions);
+        }).catch(error => {
+            logger.error(error);
+            throw new Error(error);
+        });
     }
 }
