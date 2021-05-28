29d8a7fc306b25523b89d3a666d19b0cb7b6c3f9
Henrique Delgado <32763987+henriquedelgado@users.noreply.github.com>
2021-05-28 16:27:18 +0100

    DRM-274 changes to flex flow

--- src/certificates/controllers/certificate.controller.ts
+++ src/certificates/controllers/certificate.controller.ts
@@ -26,13 +26,12 @@ export class CertificateController {
     certificate(req: Request, res: Response) {
         logger.info(`CertificateController.certificate`);
 
-        this.certificateService.getCertificate(req.params.id)
+        this.certificateService.getCertificate(req.params.id, req.query.email)
             .then((response) => {
                 logger.info(`CertificateController.certificate.then response:${JSON.stringify(response.data)}`);
 
-                const toast = req.query.redirect ? 'Congratulations on your new Skin! Check your email for details.'
-                    : req.query.active ? 'Your skin is already activated. Time to flex!' : '';                    
-
+                const toast = req.query.redirect ?
+                    'Congratulations on your new Skin! Check your email for details.': '';
 
                 let host = `${req.protocol}://${req.hostname}`
 
@@ -44,7 +43,8 @@ export class CertificateController {
                     title: 'Certificate',
                     data: response.data,
                     toast,
-                    showToast: req.query.redirect || req.query.active ? 'visible' : 'no-opacity',
+                    showToast: req.query.redirect ? 'visible' : 'no-opacity',
+                    showCTA: response.data.isOwner,
                     jsonString: JSON.stringify(response.data),
                     host: host,
                     width: ImagesConfigs.SIZES.DEFAULT * ImagesConfigs.SIZES.SCALE,
@@ -64,7 +64,43 @@ export class CertificateController {
     }
 
     /**
-     * Activate a certificate and reveal page with flashy unboxing and CTAs
+     * Assign a certificate and reveal page with a flashy box
+     * @param req
+     * @param res
+     */
+    assign(req: Request, res: Response) {
+        logger.info(`CertificateController.assign`);
+
+        this.certificateService.assignCertificate(req.query.certCode, req.query.email)
+            .then((response) => {
+                logger.info(`CertificateController.assign.then response:${JSON.stringify(response.data)}`);
+
+                let host = `${req.protocol}://${req.hostname}`
+
+                res.status(StatusCodes.OK).render('boxed', {
+                    title: 'Boxed',
+                    certCode: req.query.certCode,
+                    email: req.query.email,
+                    host: req.hostname,
+                    jsonString: JSON.stringify(response.data),
+                    width: ImagesConfigs.SIZES.DEFAULT * ImagesConfigs.SIZES.SCALE,
+                    layout: 'certificate',
+                    certificateHostPath: CertificatesRoutesConfig.ROUTE_NEEDLE
+                });
+            })
+            .catch((error: AxiosError) => {
+                logger.error(`CertificateController.assign.catch response:${error} and data: ${JSON.stringify(error.response?.data || {})}`);
+                const statusCode = error.response?.status || 500;
+
+                res.status(statusCode).render(statusCode.toString(10), {
+                    layout: 'missing-certificate', id: req.query.certCode,
+                    toast: 'Something went wrong. Please try again later.',
+                });
+            })
+    }
+
+    /**
+     * Activate a certificate and reveal page with a flashy unboxing
      * @param req
      * @param res
      */
@@ -75,23 +111,14 @@ export class CertificateController {
             .then((response) => {
                 logger.info(`CertificateController.activate.then response:${JSON.stringify(response.data)}`);
 
-                const toast = response.data.activated ? 'Congratulations on your new Skin! Check your email for details.'
-                    : 'Your skin is already activated. Time to flex!';
-
                 let host = `${req.protocol}://${req.hostname}`
 
-                if (req.hostname == 'localhost') {
-                    host = `${host}:3000`
-                }
-
-                res.status(StatusCodes.OK).render('activate', {
-                    title: 'Activation',
-                    thumbprint: req.query.certCode,
-                    toast,
-                    showToast: response.data ? 'visible' : 'no-opacity',
-                    showUnboxing: response.data.activated,
+                res.status(StatusCodes.OK).render('unboxed', {
+                    title: 'Unboxing',
+                    certCode: req.query.certCode,
+                    email: req.query.email,
+                    host: req.hostname,
                     jsonString: JSON.stringify(response.data),
-                    host: host,
                     width: ImagesConfigs.SIZES.DEFAULT * ImagesConfigs.SIZES.SCALE,
                     layout: 'certificate',
                     certificateHostPath: CertificatesRoutesConfig.ROUTE_NEEDLE
