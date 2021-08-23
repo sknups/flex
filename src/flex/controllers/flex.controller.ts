ea71d65facf32c244c48d7d2fd1fa694d89aafcf
William Griffiths <604469+wgriffiths@users.noreply.github.com>
2021-08-23 17:53:54 +0100

    DRM-68 - Add flex html page

---
+++ src/flex/controllers/flex.controller.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { logger } from "../../logger";
+import {
+  CertificateDTO,
+  CertificatesService,
+} from "../../certificates/services/certificates.service";
+
+export class FlexController {
+  private readonly certificateService: CertificatesService;
+
+  constructor() {
+    this.certificateService = new CertificatesService();
+  }
+
+
+  async getPage(request: express.Request, response: express.Response) {
+    const type = request.params.type;
+    const certCode = request.params.certCode;
+
+    try {
+      const certificateDTO = await this.getCertificate(certCode);
+      const brandCode = certificateDTO.brandCode;
+      const version = request.params.version;
+
+      logger.info(
+        `FlexController.getPage version: ${version} type: ${type} from brand: ${brandCode} with certCode: ${certCode}`
+      );
+
+     
+      const {sknappHost,flexHost,certVersion,thumbprint, stockKeepingUnitName, description } = certificateDTO;
+      response.status(StatusCodes.OK).render(`flex_${version}`, {
+        title: `${stockKeepingUnitName} - Sknups`,
+        layout: false,
+        appURL: `${sknappHost}/skn/home`,
+        cardImgUrl: `${flexHost}/skn/${certVersion}/card/default/${thumbprint}.jpg`,
+        cardThumbnailImgUrl: `${flexHost}/skn/${certVersion}/card/thumb/${thumbprint}.jpg?q=0.1`,
+        backImgUrl: `${flexHost}/skn/${certVersion}/back/default/${thumbprint}.jpg`,        
+        stockKeepingUnitName: stockKeepingUnitName,
+        ogImageUrl: `${flexHost}/skn/${certVersion}/card/og/${thumbprint}.jpg`,
+        ogUrl: `${flexHost}/flex/v1/${thumbprint}.html`,
+        twitterImageUrl: `${flexHost}/skn/${certVersion}/card/og/${thumbprint}.jpg`,
+        twitterUrl: `${flexHost}/flex/v1/${thumbprint}.html`,
+        description : description
+      
+    });
+
+    } catch (err) {
+    
+      if (err.response && err.response.status === StatusCodes.NOT_FOUND) {
+        response.writeHead(StatusCodes.NOT_FOUND);
+        response.write("Failed to get flex page - Certificate not found");
+        response.end();
+        return;
+      }      
+      logger.error(err);
+      response.writeHead(StatusCodes.INTERNAL_SERVER_ERROR);
+      response.write("Failed to get flex page");
+      response.end();
+    }
+  }
+
+  async getCertificate(withId: string): Promise<CertificateDTO> {
+    const response = await this.certificateService.getCertificate(withId);
+    return response.data;
+  }
+}
---
+++ src/flex/routes/flex.routes.config.ts
@@ -0,0 +1,29 @@
+import { CommonRoutesConfig } from "../../common/common.routes.config";
+import express from "express";
+import { FlexController } from "../controllers/flex.controller";
+import { logger } from '../../logger'
+
+export class FlexRoutesConfig extends CommonRoutesConfig {
+
+    private readonly flexController:  FlexController;
+
+    constructor(app: express.Application) {
+        super(app, 'Flex Routes');
+
+        this.flexController = new FlexController();
+    }
+
+    configureRoutes(): express.Application {       
+
+        this.getApp()
+            .route('/flex/:version/:certCode.html')
+            .get((req, res) => {                
+                this.flexController.getPage(req, res).catch((err) => {
+                    logger.error(`FlexRoutesConfig.getPage error: ${err}`);
+                });
+            });
+
+        
+        return this.getApp();
+    }
+}
