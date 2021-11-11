1128fe55f30649668c287f67bdb30e9a7f304ed3
Carlos Silva <5693046+alchimystic@users.noreply.github.com>
2021-11-11 10:07:36 +0000

    DRM-1034 some post-review fixes

--- src/templates/v1/default/Sku.ts
+++ src/templates/v1/default/Sku.ts
@@ -7,68 +7,63 @@ import { Context } from "node:vm";
 
 export class DefaultTemplate extends BrandTemplate<SkuDTO> {
 
-    async renderTemplate(dto: SkuDTO, purpose: string): Promise<Canvas> {
+    async renderTemplate(sku: SkuDTO, purpose: string): Promise<Canvas> {
         //find out if we're going to scale the image
         let scale = ImagesConfigs.SIZES.SCALE;
-        logger.debug(`Drawing card of SKU ${dto.code} purpose ${purpose}`);
-        const height = 1350;
+        logger.debug(`Drawing card of SKU ${sku.code} purpose ${purpose}`);
+
         // Load Fonts
         this.loadDefaultFontsIntoCanvas();
-        let canvas = createCanvas(900, height);
+        let canvas = createCanvas(900, 1350);
 
         const context = canvas.getContext('2d');
         context.patternQuality = 'good';
         context.quality = 'good';
 
-        const rarity = this.getSkuRarity(dto.rarity, dto.code);
+        const rarity = this.getSkuRarity(sku.rarity, sku.code);
 
         //Load all required images in parallel before drawing them on the canvas
         let images = await this.loadImages([
             `./static/backgrounds/card.front.rarity${rarity}.v3.jpg`,
-            `brand.v1.cardFront.${dto.brandCode}.png`,
-            `sku.v1.cardFront.${dto.code}.png`
+            `brand.v1.cardFront.${sku.brandCode}.png`,
+            `sku.v1.cardFront.${sku.code}.png`
         ]);
         //.then((images) => {
         //draw the images first
         const backgroundImage = images[0];
-        if (backgroundImage.status == 'fulfilled') {
+        if (backgroundImage.status === 'fulfilled') {
             context.drawImage(backgroundImage.value, 0, 0);
         } else {
             logger.info('Failed to load background image image:');
         }
         const skuImage = images[2];
-        if (skuImage.status == 'fulfilled') {
+        if (skuImage.status === 'fulfilled') {
             const imageDimensions = this.scaleToMax(900, 1350, skuImage.value);
             context.drawImage(skuImage.value, 0, 0, imageDimensions[0], imageDimensions[1]);
         } else {
-            logger.info('Failed to load sku image: ' + dto.code);
+            logger.info('Failed to load sku image: ' + sku.code);
         }
         const brandImage = images[1];
-        if (brandImage.status == 'fulfilled') {
+        if (brandImage.status === 'fulfilled') {
             const imageDimensions = this.scaleToMax(900, 1350, brandImage.value);
             context.drawImage(brandImage.value, 0, 0, imageDimensions[0], imageDimensions[1]);
         } else {
-            logger.info('Failed to load brand image: ' + dto.brandCode);
+            logger.info('Failed to load brand image: ' + sku.brandCode);
         }
 
         //write the text
         context.fillStyle = ImagesConfigs.TEXT_RGB;
         context.font = '35pt JostSemi';
         context.textAlign = 'left';
-        context.fillText(dto.name, 100, 1040);
-
-        // context.font = '35pt OCR-A';
-        // var qty = this.getItemNumberText(fromCertificate.maxQty, fromCertificate.saleQty, fromCertificate.stockKeepingUnitRarity);
-        // context.fillText('' + qty, 100, 1100);
+        context.fillText(sku.name, 100, 1040);
 
         this.writeTestWatermark(context);
-   
 
-        if (purpose == 'og') {
+        if (purpose === 'og') {
             canvas = this.convertToOg(canvas);
         }
 
-        if (purpose == 'thumb') {
+        if (purpose === 'thumb') {
             canvas = this.convertToThumb(canvas);
         }
 
