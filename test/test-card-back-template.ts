8ef59228b76c2d5780a055108f456724e5022551
Oliver Wilson <88714299+woolinsilver-sknups@users.noreply.github.com>
2022-07-11 20:57:43 +0100

    PLATFORM-2365 restructure unit tests

--- test/test-card-back-template.ts
+++ test/test-card-back-template.ts
@@ -41,110 +41,116 @@ describe('Card Back template', () => {
 
   }
 
-  interface WrapTextTestParameters extends LineBreakTestParameters {
-    text: string,
-    width?: number // pixels
-  }
-
-  /**
-   * Defines an executable unit test for the wrapText(...) method in Card Back Template.
-   */
-  class WrapTextTest extends LineBreakTest<WrapTextTestParameters> {
-
-    private static readonly DEFAULT_WIDTH = 340; // pixels
-
-    protected print(context: CanvasRenderingContext2D): void {
-      new DefaultTemplate().wrapText(context, this.params.text, 0, 0, this.params.width ?? WrapTextTest.DEFAULT_WIDTH, 0);
-    }
-
-  }
-
-  interface WriteTextTestParameters extends LineBreakTestParameters {
-    title?: string,
-    body: string,
-  }
-
-  class WriteTextTest extends LineBreakTest<WriteTextTestParameters> {
-
-    private static readonly DEFAULT_TITLE = "ITEM";
-
-    protected print(context: CanvasRenderingContext2D): void {
-      new DefaultTemplate().writeText(context, this.params.title ?? WriteTextTest.DEFAULT_TITLE, this.params.body, 0, 0, 0);
-    }
-
-  }
-
   before(() => {
     DefaultTemplate.registerFonts();
   })
 
-  it('wrapText(...) writes descriptions for Legacy SKU', () => {
+  describe('wrapText(...) method', () => {
 
-    const tests: WrapTextTest[] = [
-      new WrapTextTest({
-        text:
-            "Sculpted from the finest digital Marquina marble and accented with gold.",
-        expectation:
-            "Sculpted from the finest digital \n" +
-            "Marquina marble and accented with \n" +
-            "gold. "
-      }),
-      new WrapTextTest({
-        text:
-            "The Dao-Disk gloves are the key to unlocking Cybermonk's inner journey. They provide insulation and protection from the elements. However, the most important function is the Dao-Disk, which serves as a digital prayer wheel.",
-        expectation:
-            "The Dao-Disk gloves are the key to \n" +
-            "unlocking Cybermonk's inner \n" +
-            "journey. They provide insulation \n" +
-            "and protection from the elements. \n" +
-            "However, the most important \n" +
-            "function is the Dao-Disk, which \n" +
-            "serves as a digital prayer wheel. "
-      }),
-      new WrapTextTest({
-        text:
-            "Feel the faux fur fuzz. This Fuzz Dome hat in Fuchsia colourway is one of six designs for Benny Andallo's first-ever digital collection.",
-        expectation:
-            "Feel the faux fur fuzz. This Fuzz \n" +
-            "Dome hat in Fuchsia colourway is \n" +
-            "one of six designs for Benny \n" +
-            "Andallo's first-ever digital \n" +
-            "collection. "
-      })
-    ];
-
-    for (const test of tests) {
-      test.execute()
+    interface WrapTextTestParameters extends LineBreakTestParameters {
+      text: string,
+      width?: number // pixels
     }
 
+    /**
+     * Defines an executable unit test for the wrapText(...) method in Card Back Template.
+     */
+    class WrapTextTest extends LineBreakTest<WrapTextTestParameters> {
+
+      private static readonly DEFAULT_WIDTH = 340; // pixels
+
+      protected print(context: CanvasRenderingContext2D): void {
+        new DefaultTemplate().wrapText(context, this.params.text, 0, 0, this.params.width ?? WrapTextTest.DEFAULT_WIDTH, 0);
+      }
+
+    }
+
+    it('writes descriptions for Legacy SKU', () => {
+
+      const tests: WrapTextTest[] = [
+        new WrapTextTest({
+          text:
+              "Sculpted from the finest digital Marquina marble and accented with gold.",
+          expectation:
+              "Sculpted from the finest digital \n" +
+              "Marquina marble and accented with \n" +
+              "gold. "
+        }),
+        new WrapTextTest({
+          text:
+              "The Dao-Disk gloves are the key to unlocking Cybermonk's inner journey. They provide insulation and protection from the elements. However, the most important function is the Dao-Disk, which serves as a digital prayer wheel.",
+          expectation:
+              "The Dao-Disk gloves are the key to \n" +
+              "unlocking Cybermonk's inner \n" +
+              "journey. They provide insulation \n" +
+              "and protection from the elements. \n" +
+              "However, the most important \n" +
+              "function is the Dao-Disk, which \n" +
+              "serves as a digital prayer wheel. "
+        }),
+        new WrapTextTest({
+          text:
+              "Feel the faux fur fuzz. This Fuzz Dome hat in Fuchsia colourway is one of six designs for Benny Andallo's first-ever digital collection.",
+          expectation:
+              "Feel the faux fur fuzz. This Fuzz \n" +
+              "Dome hat in Fuchsia colourway is \n" +
+              "one of six designs for Benny \n" +
+              "Andallo's first-ever digital \n" +
+              "collection. "
+        })
+      ];
+
+      for (const test of tests) {
+        test.execute()
+      }
+
+    });
+
+    it('allows a large word on the first line to exceed maximum width', () => {
+      new WrapTextTest({
+        width: 10, // pixels
+        text:
+            "supercalifragilisticexpialidocious",
+        expectation:
+            "supercalifragilisticexpialidocious "
+      }).execute();
+    });
+
+    it('allows a large word on a subsequent line to exceed maximum width', () => {
+      new WrapTextTest({
+        width: 280, // pixels
+        text:
+            "If you say it loud enough you'll always sound precocious, supercalifragilisticexpialidocious",
+        expectation:
+            "If you say it loud enough \n" +
+            "you'll always sound \n" +
+            "precocious, \n" +
+            "supercalifragilisticexpialidocious "
+      }).execute();
+    });
+
   });
 
-  it('wrapText(...) allows a large word on the first line to exceed maximum width', () => {
-    new WrapTextTest({
-      width: 10, // pixels
-      text:
-          "supercalifragilisticexpialidocious",
-      expectation:
-          "supercalifragilisticexpialidocious "
-    }).execute();
-  });
+  describe('writeText(...) method', () => {
 
-  it('wrapText(...) allows a large word on a subsequent line to exceed maximum width', () => {
-    new WrapTextTest({
-      width: 280, // pixels
-      text:
-          "If you say it loud enough you'll always sound precocious, supercalifragilisticexpialidocious",
-      expectation:
-          "If you say it loud enough \n" +
-          "you'll always sound \n" +
-          "precocious, \n" +
-          "supercalifragilisticexpialidocious "
-    }).execute();
-  });
+    interface WriteTextTestParameters extends LineBreakTestParameters {
+      title?: string,
+      body: string,
+    }
 
-  it('writeText(...) writes names for Legacy SKU', () => {
+    class WriteTextTest extends LineBreakTest<WriteTextTestParameters> {
 
-    const tests: WriteTextTest[] = [
+      private static readonly DEFAULT_TITLE = "ITEM";
+
+      protected print(context: CanvasRenderingContext2D): void {
+        new DefaultTemplate().writeText(context, this.params.title ?? WriteTextTest.DEFAULT_TITLE, this.params.body, 0, 0, 0);
+      }
+
+    }
+
+    it('writes names for Legacy SKU', () => {
+
+      const tests: WriteTextTest[] = [
         new WriteTextTest({
           body:
               "Baker Crown Juicy",
@@ -173,12 +179,14 @@ describe('Card Back template', () => {
               "Wonky Dome Moo \n" +
               "Moo ",
         }),
-    ];
+      ];
 
-    for (const test of tests) {
-      test.execute();
-    }
+      for (const test of tests) {
+        test.execute();
+      }
 
-  })
+    });
+
+  });
 
 })
