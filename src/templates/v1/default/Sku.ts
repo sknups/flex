4cb7ba382e2123f6a1d740523bef7d2aabf22ea4
Oliver Wilson <oliver.wilson@sknups.com>
2022-08-11 13:58:32 +0100

    PLATFORM-2569 separate code paths for single-line / multi-line text

--- src/templates/v1/default/Sku.ts
+++ src/templates/v1/default/Sku.ts
@@ -16,8 +16,6 @@ export class DefaultTemplate extends BrandTemplate<SkuDTO> {
     static readonly SKU_NAME_STYLE = {
         color: ImagesConfigs.TEXT_COLOR,
         font: '35pt "Jost" Semibold',
-        lineHeight: 0,
-        maximumWidth: Infinity, // wrapping disabled
         align: 'left'
     };
 
@@ -38,7 +36,7 @@ export class DefaultTemplate extends BrandTemplate<SkuDTO> {
           // print SKU name (original case preserved)
           this.print(context, DefaultTemplate.SKU_NAME_STYLE, sku.name, DefaultTemplate.TEXT_X, DefaultTemplate.SKU_NAME_BASELINE);
         }
-        
+
         this.writeTestWatermark(context);
 
         return canvas;
